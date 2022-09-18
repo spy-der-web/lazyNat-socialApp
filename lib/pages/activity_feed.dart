7c86c5fdd53083661f561feeeedb7752cbe4e497
spy-der-web <joy.obor.247589@unn.edu.ng>
2022-09-18 06:12:01 +0100

    CONFUSED

---
+++ lib/pages/activity_feed.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ActivityFeed extends StatefulWidget {
+  @override
+  _ActivityFeedState createState() => _ActivityFeedState();
+}
+
+class _ActivityFeedState extends State<ActivityFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Activity Feed');
+  }
+}
+
+class ActivityFeedItem extends StatelessWidget {
+  const ActivityFeedItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Activity Feed Item');
+  }
+}
